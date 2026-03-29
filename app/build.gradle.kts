ce6068e4029e4130f837a60495fad99a409f4cdf
Lumince <jmrjmr1997@gmail.com>
2026-03-29 17:23:01 -0400

    update version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.veygax.eventhorizon"
         minSdk = 24
         targetSdk = 36
-        versionCode = 16
-        versionName = "2.3.5.3"
+        versionCode = 17
+        versionName = "2.3.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
